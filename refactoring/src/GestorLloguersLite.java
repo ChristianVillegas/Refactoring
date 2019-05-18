8e9648286f0ac9105573960395fdf13608da5fa1
Christian <Christian@192.168.56.1>
2019-05-18 22:17:49 +0200

    Exercicis 2 i 3(sense acabar)

---
+++ refactoring/src/GestorLloguersLite.java
@@ -0,0 +1,25 @@
+
+public class GestorLloguersLite {
+
+	public static void main(String[] args) {
+		// construcció d'un client, amb el meu nom
+		Client c1 = new Client("12334569-C", "Christian", "622454554");
+		
+		// construcció d'un vehicle de categoria BASIC
+		Vehicle vehicleBasic = new Vehicle("Tata", "Vista", Vehicle.BASIC);
+
+		// construccuó d'un lloguer amb una data
+		SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+		Date date = dateFormat.parse("2/8/2013");
+		Lloguer lloguerBasic = new Lloguer(date, 2, vehicleBasic);
+
+		// formatat d'una data
+		System.out.println(dateFormat.format(lloguerBasic.getData()));
+		
+		public static String mostrar(Client c1) {
+			return System.out.println("Client: " + client.c1 + "Lloguers: ");
+		}
+
+	}
+
+}
---
+++ refactoring/src/Lloguer.java
@@ -0,0 +1,17 @@
+
+public class Lloguer {
+	private int data;
+	private int dies;
+	
+	public Lloguer(int data, int dies) {
+		this.data = data;
+		this.dies = dies;
+	}
+	
+	public int getData()		{ return data;	}
+	public int getDies()		{ return dies;  }
+	
+	public void setData(int data) { this.data = data; }
+	public void setDies(int dies) { this.dies = dies; }
+
+}
