530f992eb6516a9eb7ff2e3cc8fdfbf1cf27418d
Christian Villegas <cf18christian.villegas@iesjoandaustria.org>
2019-05-20 19:03:12 +0200

    Exercici 3 acabat

--- refactoring/src/GestorLloguersLite.java
+++ refactoring/src/GestorLloguersLite.java
@@ -1,25 +1,46 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class GestorLloguersLite {
 
 	public static void main(String[] args) {
-		// construcciÛ d'un client, amb el meu nom
+		// construcci√≥ d'un client, amb el meu nom
 		Client c1 = new Client("12334569-C", "Christian", "622454554");
 		
-		// construcciÛ d'un vehicle de categoria BASIC
+		// construcci√≥ d'un vehicle de categoria BASIC
 		Vehicle vehicleBasic = new Vehicle("Tata", "Vista", Vehicle.BASIC);
 
-		// construccuÛ d'un lloguer amb una data
+		// construcci√≥ d'un lloguer amb una data
 		SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
-		Date date = dateFormat.parse("3/8/2013");
+		Date date = null;
+		try {
+			date = dateFormat.parse("3/8/2018");
+		} catch (Exception e) {
+			System.out.println("Error! Format incorrecte.");
+		}
 		Lloguer lloguerBasic = new Lloguer(date, 2, vehicleBasic);
+		
+		c1.afegeix(lloguerBasic);
 
 		// formatat d'una data
 		System.out.println(dateFormat.format(lloguerBasic.getData()));
 		
-		public static String mostrar(Client c1, Lloguer) {
-			return System.out.println("Client: " + client.c1 + "Lloguers: " + Lloguer);
-		}
-
+		// crida m√®tode mostrar, on mostrar√† totes les dades
+		String informacio = mostrar(c1);
+		System.out.println(informacio);
 	}
-
+	
+	public static String mostrar(Client client) {
+		String informacio = "";
+		informacio += "Client: " + client.getNom() + "\n";
+		informacio += "\t" + client.getNif() + "\n";
+		informacio += "\t" + client.getTelefon() + "\n";
+		informacio += "Lloguers: " + client.getLloguers().size() + "\n";
+		for (int i = 0; i < client.getLloguers().size(); i++) {
+			informacio += "\t" + (i+1) + ". vehicle: " + client.getLloguers().get(i).getVehicle().getMarca() + " " + client.getLloguers().get(i).getVehicle().getModel() + "\n";
+			informacio += "\t   data d'inici: " + client.getLloguers().get(i).getData() + "\n";
+			informacio += "\t   dies llogats: " + client.getLloguers().get(i).getDies() + "\n";
+		}
+		return informacio;
+	}
 }
