315ce8ef65b01301bbbdb5d081d0b85046bf1887
Christian Villegas <cf18christian.villegas@iesjoandaustria.org>
2019-05-22 19:00:36 +0200

    Exercici 8

--- refactoring/src/Client.java
+++ refactoring/src/Client.java
@@ -37,29 +37,6 @@ public class Client {
         return lloguers.contains(lloguer);
     }
     
-    private double quantitatPerLloguer(Lloguer lloguer) {
-    	double quantitat = 0;
-    	
-    	switch (lloguer.getVehicle().getCategoria()) {
-        case Vehicle.BASIC:
-            quantitat += 3;
-            if (lloguer.getDies() > 3) {
-                quantitat += (lloguer.getDies() - 3) * 1.5;
-            }
-            break;
-        case Vehicle.GENERAL:
-            quantitat += 4;
-            if (lloguer.getDies() > 2) {
-                quantitat += (lloguer.getDies() - 2) * 2.5;
-            }
-            break;
-        case Vehicle.LUXE:
-            quantitat += lloguer.getDies() * 6;
-            break;  
-    	}
-    	return quantitat;
-    }
-    
     public String informe() {
     	  double total = 0;
     	    int bonificacions = 0;
@@ -67,7 +44,7 @@ public class Client {
     	        getNom() +
     	        " (" + getNif() + ")\n";
     	    for (Lloguer lloguer: lloguers) {
-    	        double quantitat = quantitatPerLloguer(lloguer);
+    	        double quantitat = lloguer.quantitat();
 
     	        // afegeix lloguers freqüents
     	        bonificacions ++;
--- refactoring/src/Lloguer.java
+++ refactoring/src/Lloguer.java
@@ -19,4 +19,27 @@ public class Lloguer {
 	public void setDies(int dies) { this.dies = dies; }
 	public void setVehiche(Vehicle vehicle) { this.vehicle = vehicle; }
 
+	 public double quantitat() {
+		 //Amb aquest mètode no agafa el paràmetre lloguer perqué ja ens trobem a la classe 'Lloguer' i només ha d'agafar els seus atributs
+	    	double quantitat = 0;
+	    	
+	    	switch (vehicle.getCategoria()) {
+	        case Vehicle.BASIC:
+	            quantitat += 3;
+	            if (dies > 3) {
+	                quantitat += (dies - 3) * 1.5;
+	            }
+	            break;
+	        case Vehicle.GENERAL:
+	            quantitat += 4;
+	            if (dies > 2) {
+	                quantitat += (dies - 2) * 2.5;
+	            }
+	            break;
+	        case Vehicle.LUXE:
+	            quantitat += dies * 6;
+	            break;  
+	    	}
+	    	return quantitat;
+	    }
 }
